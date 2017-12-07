5dbe39d2e063abd820a0e92133a7c320304ed249
Stef Bon <stefbon@gmail.com>
2017-12-07 08:39:31 +0100

    Remove unused declarations.

--- ssh/ssh-receive.h
+++ ssh/ssh-receive.h
@@ -22,13 +22,10 @@
 
 typedef void (* receive_msg_cb_t)(struct ssh_session_s *session, struct ssh_payload_s *payload);
 
-int get_payload_ssh_data(struct rawdata_s *data, struct ssh_payload_s *payload);
 void process_ssh_message(struct ssh_session_s *session, struct ssh_payload_s *payload);
 
 int init_receive(struct ssh_session_s *session, pthread_mutex_t *mutex, pthread_cond_t *cond, unsigned int *error);
 void free_receive(struct ssh_session_s *session);
-
-void start_processing_queue(struct ssh_session_s *session);
 
 void register_msg_cb(unsigned char type, receive_msg_cb_t cb);
 
