f2eb216218576de6a3f713efd29338b264014fed
Stef Bon <stefbon@gmail.com>
2018-04-10 12:05:00 +0200

    Move ssh specific types to dedicated directory.

--- ssh/ssh-datatypes.h
+++ ssh/ssh-datatypes.h
@@ -20,48 +20,11 @@
 #ifndef FS_WORKSPACE_SSH_DATATYPES_H
 #define FS_WORKSPACE_SSH_DATATYPES_H
 
-#if HAVE_LIBGCRYPT
-#include <gcrypt.h>
-#endif
-
-struct ssh_string_s {
-    unsigned int			len;
-    char				*ptr;
-};
-
-struct commalist_s {
-    char 				*list;
-    unsigned int 			len;
-    unsigned int 			size;
-};
-
-struct ssh_pkalgo_s {
-    unsigned int			id;
-    const char				*name;
-    unsigned int			len;
-};
-
-struct ssh_mpint_s {
-    union {
-#if HAVE_LIBGCRYPT
-	gcry_mpi_t			mpi;
-#endif
-	void				*ptr;
-    } lib;
-};
+#include "datatypes/ssh-string.h"
+#include "datatypes/ssh-namelist.h"
+#include "datatypes/ssh-uint.h"
+#include "datatypes/ssh-mpint.h"
 
 /* prototypes */
-
-void init_ssh_string(struct ssh_string_s *s);
-void free_ssh_string(struct ssh_string_s *s);
-unsigned int create_ssh_string(struct ssh_string_s *s, unsigned int len);
-int get_ssh_string_from_buffer(char **b, unsigned int size, struct ssh_string_s *s);
-unsigned int write_ssh_string(char *buffer, unsigned int size, const unsigned char type, void *ptr);
-
-int read_pk_mpint(struct ssh_mpint_s *mp, char *buffer, unsigned int size, unsigned int *error);
-int write_pk_mpint(struct ssh_mpint_s *mp, char *buffer, unsigned int size, unsigned int *error);
-
-void free_pk_mpint(struct ssh_mpint_s *mp);
-void init_pk_mpint(struct ssh_mpint_s *mp);
 
 #endif
